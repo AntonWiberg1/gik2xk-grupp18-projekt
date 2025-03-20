880ccbc809ef773e7a59f1c037f6afe28689a43b
AntonWiberg1 <anton.wiberg@live.se>
2025-03-20 12:38:16 +0100

    Lagt till kommentar i Cart

--- client/src/views/Cart.jsx
+++ client/src/views/Cart.jsx
@@ -1,5 +1,9 @@
 function Cart() {
-  return ( <h2>Cart</h2> );
+  return ( 
+  <>  
+    <h2>Cart</h2>
+    <p>Här ska man få upp sin varukorg baserat på kundID, kunna ta bort varor, lägga till flera eller minska antal med hjälp av någon counter, gå till checkout</p>
+  </> );
 }
 
 export default Cart;
