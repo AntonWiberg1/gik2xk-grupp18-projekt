ad55fa84d58c736c89cf375401f48676c7935551
KarWah <147503015+KarWah@users.noreply.github.com>
2025-03-20 13:43:38 +0100

    push

--- client/src/components/ProductItemSmall.jsx
+++ client/src/components/ProductItemSmall.jsx
@@ -1,13 +1,19 @@
+import {Link} from 'react-router-dom'
+
 function ProductItemSmall ({product}){
-    return<>
-        <h3>{product.title}</h3>
-                        <p>Price: ${product.price}</p>
-                        <p>Reviews:</p>
-                        <ul>
-                            {product.ratings.map(rating => (
-                                <li key={rating.id}>Rating: {rating.rating}</li>
-                            ))}
-                        </ul>
-    </>;
+    return(
+        <>
+            <Link>
+            <h3>{product.title}</h3>
+            </Link>
+                <p>Price: ${product.price}</p>
+                <p>Reviews:</p>
+                <ul>
+                    {product.ratings.map(rating => (
+                    <li key={rating.id}>Rating: {rating.rating}</li>
+                    ))}
+                </ul>
+    </>
+    );
 }
 export default ProductItemSmall;
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,11 +1,20 @@
 import ProductList from "../components/ProductList";
+import CartList from "../components/CartList"
+import {Grid} from '@mui/material';
 
 function Home() {
 
   return (
-    <> 
-      <h2>HOMEPAGE</h2> 
+    <>
+    <Grid container spacing = {2}>
+      <Grid item xs ={12} sm={8}>
       <ProductList />
+      </Grid>
+      <Grid item xs ={12} sm={4}>
+        <CartList />
+        
+      </Grid>
+    </Grid>
     </>
 );
 
