1a07cf919830a1ee0782f56b7efafd2234c2148a
AntonWiberg1 <anton.wiberg@live.se>
2025-03-20 11:47:07 +0100

    Lagt in routemappning mellan komponenter i APP.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,40 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ProductEdit from './views/ProductEdit.jsx';
+import ProductDetail from './views/ProductDetail.jsx';
+import Home from './views/Home.jsx';
+import Products from './views/Products.jsx';
+
+const router = createBrowserRouter([
+  { 
+    path: '/', 
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/products/new', 
+        element: <ProductEdit />
+      },
+      {
+        path: '/products/', 
+        element: <Products />
+      },
+      {
+        path: '/products/1', 
+        element: <ProductDetail />
+      }
+    ]
+  }
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
