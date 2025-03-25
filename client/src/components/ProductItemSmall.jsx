f0bb309d4a38967eab2a26070ec012538c3131d0
SimRuo <133385216+SimRuo@users.noreply.github.com>
2025-03-25 10:00:09 +0100

    added cart funcitons

--- client/src/components/ProductItemSmall.jsx
+++ client/src/components/ProductItemSmall.jsx
@@ -1,55 +1,51 @@
-import { Link } from 'react-router-dom';
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import HoverRating from './HoverRating';
+import { Link } from "react-router-dom";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import HoverRating from "./HoverRating";
+import { addOne } from "../services/CartService";
 
 function ProductItemSmall({ product }) {
-    return (
-        <>
-
-<Card sx={{ mb: 3, maxWidth: 250 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="\src\assets\react.svg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        <Link to={`/products/${product.id}`} style={{ textDecoration: "none", color: "inherit" }}>
-                <h4>{product.title}</h4>
+  return (
+    <>
+      <Card sx={{ mb: 3, maxWidth: 250 }}>
+        <CardMedia component="img" alt="green iguana" height="140" image="\src\assets\react.svg" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            <Link to={`/products/${product.id}`} style={{ textDecoration: "none", color: "inherit" }}>
+              <h4>{product.title}</h4>
             </Link>
-        </Typography>
-        
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-        <p>Price: ${product.price}</p>
+          </Typography>
+
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            <p>Price: ${product.price}</p>
             {product.amount && <p>Amount: {product.amount}</p>} {/* Display amount if it exists */}
             {product.ratings && ( // Check if ratings exist
-                <>
-                    <p>Reviews:</p>
-                    <ul>
-                        {product.ratings.map(rating => (
-                            <li key={rating.id}>Rating: {rating.rating}</li>
-                        ))}
-                    </ul>
-                </>
+              <>
+                <p>Reviews:</p>
+                <ul>
+                  {product.ratings.map((rating) => (
+                    <li key={rating.id}>Rating: {rating.rating}</li>
+                  ))}
+                </ul>
+              </>
             )}
-        </Typography>
-        <HoverRating />
-      </CardContent>
-      <CardActions>
-        <Button size="small">Lägg i varukorg</Button>
-        <Button size="small">Läs mer</Button>
-      </CardActions>
-    </Card>
-            
-        </>
-    );
+          </Typography>
+          <HoverRating />
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={() => addOne(product.id)}>
+            Lägg i varukorg
+          </Button>
+          <Button size="small">Läs mer</Button>
+        </CardActions>
+      </Card>
+    </>
+  );
 }
 
 export default ProductItemSmall;
--- client/src/services/CartService.js
+++ client/src/services/CartService.js
@@ -30,3 +30,24 @@ export async function getOne(id) {
         e?.response ? console.log(e.response.data) : console.log(e);
     }
 }
+
+export async function addOne(productId) {
+    const userId = 3;
+    const amount = 1;
+    try {
+        const response = await axios.post(`/carts/addProduct/`, {
+            userId,
+            productId,
+            amount
+        });
+
+        if (response.status === 200) return response.data;
+        else {
+            console.log(response);
+            return null;
+        }
+    }
+    catch (e) {
+        e?.response ? console.log(e.response.data) : console.log(e);
+    }
+}
