56c89063fde203eb7952445dc23ebcdddbb99a45
SimRuo <133385216+SimRuo@users.noreply.github.com>
2025-03-25 12:34:34 +0100

    Styling

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -5,11 +5,10 @@ import { Grid2 } from "@mui/material";
 function Home() {
   return (
     <>
-      <Grid2 container spacing={2} sx={{ display: "flex" }}>
+      <Grid2 container spacing={2} sx={{ display: "flex", pt: "80px" }}>
         <Grid2 item xs={12} sm={6}>
           <ProductList />
         </Grid2>
-        <Grid2 item xs={12} sm={4}></Grid2>
       </Grid2>
     </>
   );
